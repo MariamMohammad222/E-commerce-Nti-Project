28047b9f9619939b4e2002eb04908430fa3ead10
Mariam Mohamed <mariammtaha2003@example.com>
2025-12-22 12:59:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+Hello Created By Abdalluh Essam 🇪🇬🇪🇬🇪
+abdallhesam100@gmail.com
+
+
+
+
+# nti_project_final
+
+
+🚀 Clean Architecture Flutter Project Generated Automatically
+
+
+## Structure
+
+
+```
+lib/
+├── core/
+│   ├── constants
+│   ├── network
+│   ├── errors
+│   ├── utils
+│   ├── services
+│   ├── routing
+│   ├── theme
+│   ├── cubit
+│   └── extensions
+├── features/
+│   ├── splash
+│   ├── onboarding
+│   ├── auth
+│   └── home
+├── app.dart
+├── main.dart
+└── app_bloc_observer.dart
+```
+
+
+## Getting Started
+```bash
+flutter pub get
+flutter run
+```
+
+
+---
+
+
+✅ Built with ❤️ using the Clean Architecture Generator
+
+
+Hello Created By Abdalluh Essam 🇪🇬🇪🇬🇪
---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../routing/routes.dart';
+import '../../features/splash/presentation/screens/splash_screen.dart';
+import '../../features/onboarding/presentation/screens/onboarding_screen.dart';
+
+
+class AppRouter {
+ Route? generateRoute(RouteSettings settings) {
+   switch (settings.name) {
+     case Routes.splashScreen:
+       return _createRoute(const SplashScreen());
+     case Routes.onBoardingScreen:
+       return _createRoute(const OnboardingScreen());
+
+
+     default:
+       return null;
+   }
+ }
+
+
+ PageRouteBuilder _createRoute(Widget page) {
+   return PageRouteBuilder(
+     transitionDuration: const Duration(milliseconds: 400),
+     pageBuilder: (context, animation, secondaryAnimation) => page,
+     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+       return FadeTransition(
+         opacity: animation,
+         child: child,
+       );
+     },
+   );
+ }
+}
